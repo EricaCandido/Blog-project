031cf3b455a840349342b6c2d39663413b8e7d1c
EricaCandido <ericandido1997@gmail.com>
2023-04-06 17:34:37 +0200

    06-04-2023

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import Layout from "@/components/layout";
+
+const About = () => {
+  return (
+    <Layout>
+      <h1>About section, work in progress..</h1>
+    </Layout>
+  );
+};
+
+export default About;
