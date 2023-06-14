2eaa7bb08b060cb202004ef84e8db9e2547ca599
EricaCandido <ericandido1997@gmail.com>
2023-06-14 09:25:43 +0200

    14-06-2023

--- src/pages/event/[id].jsx
+++ src/pages/event/[id].jsx
@@ -1,34 +1,34 @@
-import Layout from "@/components/layout";
+// import Layout from "@/components/layout";
 
-import { getAllIds, getNewPostData } from "@/utils/funcs";
+// import { getAllIds, getNewPostData } from "@/utils/funcs";
 
-export default function NewPost(props) {
-  const { title, reactions, body } = props.newPostData;
-  console.log(newPostData);
-  return (
-    <Layout>
-      <h1>{title}</h1>
-      <p>{body}</p>
-      <div>{reactions}❤️</div>
-    </Layout>
-  );
-}
+// export default function NewPost(props) {
+//   const { title, reactions, body } = props.newPostData;
+//   console.log(newPostData);
+//   return (
+//     <Layout>
+//       <h1>{title}</h1>
+//       <p>{body}</p>
+//       <div>{reactions}❤️</div>
+//     </Layout>
+//   );
+// }
 
-export const getStaticPaths = () => {
-  const paths = getAllIds();
-  return {
-    paths,
-    fallback: false,
-  };
-};
+// export const getStaticPaths = () => {
+//   const paths = getAllIds();
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
 
-export const getStaticProps = ({ params }) => {
-  const newPostData = getNewPostData(params.id);
+// export const getStaticProps = ({ params }) => {
+//   const newPostData = getNewPostData(params.id);
 
-  console.log(newPostData);
-  return {
-    props: {
-      newPostData,
-    },
-  };
-};
+//   console.log(newPostData);
+//   return {
+//     props: {
+//       newPostData,
+//     },
+//   };
+// };
